50026fe32ce8b87d71f500c2ac697c433e2eaff1
Levi Barsis <lbarsis@gmail.com>
2022-10-01 16:04:24 -0700

    Done with assignment including bonus

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,33 @@
 import React from "react";
 
-function Article({title, date="January 1, 1970", preview}) {
+function Article({title, date="January 1, 1970", preview, minutes}) {
+
+  function TimeToRead() {
+    if (minutes < 30) {
+      const coffee = []
+      const numOfCoffee = Math.ceil(minutes / 5)
+
+      for (let i = 0; i < numOfCoffee; i++) {
+        coffee.push('☕️')
+      }
+
+      return <small> * {minutes} minutes to read {coffee.join('')}</small>
+    } else  if (minutes >= 30){
+      const bento = []
+      const numOfBento = Math.ceil(minutes / 10)
+
+      for (let i = 0; i < numOfBento; i++) {
+        bento.push('🍱')
+      }
+      return <small> * {minutes} minutes to read {bento.join('')}</small>
+    }
+  }
+
   return(
     <article>
       <h3>{title}</h3>
       <small>{date}</small>
+        <TimeToRead />
       <p>{preview}</p>
     </article>
   )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,7 +4,7 @@ import Article from "./Article";
 function ArticleList({posts}) {
   const postEls = posts.map(post => {
     return <article key={post.id}>
-      <Article title={post.title} date={post.date} preview={post.preview}/>
+      <Article title={post.title} date={post.date} preview={post.preview} minutes={post.minutes}/>
     </article>
   })
 
