8ea0368974dca0145804568d46b6a1eb45d26583
Levi Barsis <lbarsis@gmail.com>
2022-10-01 16:06:39 -0700

    Done with assignment including bonus but commented out for passing tests

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,32 +2,32 @@ import React from "react";
 
 function Article({title, date="January 1, 1970", preview, minutes}) {
 
-  function TimeToRead() {
-    if (minutes < 30) {
-      const coffee = []
-      const numOfCoffee = Math.ceil(minutes / 5)
+  // function TimeToRead() {
+  //   if (minutes < 30) {
+  //     const coffee = []
+  //     const numOfCoffee = Math.ceil(minutes / 5)
 
-      for (let i = 0; i < numOfCoffee; i++) {
-        coffee.push('☕️')
-      }
+  //     for (let i = 0; i < numOfCoffee; i++) {
+  //       coffee.push('☕️')
+  //     }
 
-      return <small> * {minutes} minutes to read {coffee.join('')}</small>
-    } else  if (minutes >= 30){
-      const bento = []
-      const numOfBento = Math.ceil(minutes / 10)
+  //     return <small> * {minutes} minutes to read {coffee.join('')}</small>
+  //   } else  if (minutes >= 30){
+  //     const bento = []
+  //     const numOfBento = Math.ceil(minutes / 10)
 
-      for (let i = 0; i < numOfBento; i++) {
-        bento.push('🍱')
-      }
-      return <small> * {minutes} minutes to read {bento.join('')}</small>
-    }
-  }
+  //     for (let i = 0; i < numOfBento; i++) {
+  //       bento.push('🍱')
+  //     }
+  //     return <small> * {minutes} minutes to read {bento.join('')}</small>
+  //   }
+  // }
 
   return(
     <article>
       <h3>{title}</h3>
       <small>{date}</small>
-        <TimeToRead />
+      {/* <TimeToRead /> */}
       <p>{preview}</p>
     </article>
   )
