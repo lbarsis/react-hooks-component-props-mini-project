3440dbcdd302fac3f72b1f34e8d1067908dfd0d0
Levi Barsis <lbarsis@gmail.com>
2022-10-01 15:35:15 -0700

    Done with main portion of Assignment, bonus material next

---
+++ src/components/ArticleList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}) {
+  const postEls = posts.map(post => {
+    return <article key={post.id}>
+      <Article title={post.title} date={post.date} preview={post.preview}/>
+    </article>
+  })
+
+  return (
+    <main>
+      {postEls}
+    </main>
+  )
+}
+
+export default ArticleList
